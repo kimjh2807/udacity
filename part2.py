fa24225ed8311c21dd7c45326a9146468273c5a5
kimjh2807 <kimjh2807@gmail.com>
2020-12-26 17:36:23 +0900

    Create 20201226

---
+++ part2.py
@@ -0,0 +1,18 @@
+# PART 2
+# Lesson 4: Data Analysis Process - Case Study
+
+# import packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+wine_red = pd.read_csv('data/winequality-red.csv', sep = ';')
+print(wine_red.info())
+print(wine_white.info())
+print
+
+# visualize
+#wine_red.plot['fixed_acidity'].hist()
+#plt.plot([1, 2, 3, 4])
+#plt.ylabel('some numbers')
+#plt.show()
